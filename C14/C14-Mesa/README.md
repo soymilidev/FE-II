be3eeed0da5e9cdb7a89005ae4be1aa08c8e5411
soymilidev <milicastro.dev@gmail.com>
2023-02-09 19:29:02 -0300

    Creacion de archivo C14

---
+++ C14/C14-Mesa/README.md
@@ -0,0 +1,12 @@
+
+# Lista de comentarios
+
+A partir de la maqueta asignada, cumplir con los siguientes requerimientos:
+
+- Utilizar el formulario para captar el texto ingresado.
+- Implementar el evento "submit", utilizarlo para guardar el comentario en un array.
+- Cada vez que se agrega un nuevo comentario renderizarlo en una etiqueta "p"(sacar del HTML los hardcodeados y hacerlo dinámico).
+- Constantemente guardar la información en localStorage, si se recarga la página deberían mantenerse los comentarios.
+
+### Prewiev
+![Preview](https://github.com/soymilidev/FE-II/blob/main/C14/C14-Mesa/img/img-form.png)
---
+++ C14/C14-Mesa/js/index.js
@@ -0,0 +1,45 @@
+/*
+REQUERIMIENTOS
+- Utilizar el formulario para captar el texto ingresado
+- Implementar el evento "submit", utilizarlo para guardar el comentario en un array
+- Cada vez que se agrega un nuevo comentario renderizarlo en una etiqueta "p"(sacar del HTML los hardcodeados y hacerlo dinámico)
+- Constantemente guardar la información en localStorage, si se recarga la página deberían mantenerse los comentarios
+*/
+
+
+const form = document.querySelector("form");
+const comentarios = document.querySelector(".comentarios-box");
+
+//Espacio para almacenar los comentarios.
+let listaComentarios = [];
+
+//Comprobar si hay algún valor guardado en el almacenamiento local.
+//getItem --> devuelve el valor de la clave cuyo nombre se le pasa por parámetro
+if (localStorage.getItem("comentarios")) {
+    //Si hay algún valor guardado en el almacenamiento local, se carga en "listaComentarios" usando JSON.parse().
+    listaComentarios = JSON.parse(localStorage.getItem("comentarios"));
+    //Mostrar los comentarios guardados en la página
+    renderizarComentarios();
+}
+
+
+form.addEventListener("submit", function(event) {
+    //Cuando se envía un nuevo comentario (se previene su envío) se agrega al final de la lista de comentarios.
+    event.preventDefault();
+    const comentario = document.querySelector("#comentario-input").value;
+    listaComentarios.push(comentario);
+    //Mostrar el nuevo comentario
+    renderizarComentarios();
+    localStorage.setItem("comentarios", JSON.stringify(listaComentarios));
+});
+
+//Mostrar todos los comentarios, recorrer la lista de coment. y crear un p para c/u
+//Luego, se guarda la lista actualizada de comentarios en el almacenamiento local.
+function renderizarComentarios() {
+    comentarios.innerHTML = "";
+    listaComentarios.forEach(function(comentario) {
+        const p = document.createElement("p");
+        p.textContent = comentario;
+        comentarios.appendChild(p);
+    });
+}
